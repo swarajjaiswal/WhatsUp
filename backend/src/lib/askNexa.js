5d4a8c45402fd5705b63c72291d66e4f0c48306d
swarajjaiswal <swaraj93310@gmail.com>
2026-01-01 23:55:04 +0530

    updated gemini version

--- backend/src/lib/askNexa.js
+++ backend/src/lib/askNexa.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "models/gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "models/gemini-2.5-flash" });
 
 export async function askNexa(message, userName = "User") {
   try {
